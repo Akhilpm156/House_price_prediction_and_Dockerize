4c6816183040f187a71501d4073457934854b2bf
Akhil P M <140160184+Akhilpm156@users.noreply.github.com>
2024-04-16 14:20:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def predict():
     return render_template('index.html', prediction=prediction[0])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0')
